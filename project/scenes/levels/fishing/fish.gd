3c4574eeeb2f9570e8aa9dcf635ffb1f91c3fd89
Aidan Fleischer <aidan@Aidans-MacBook-Air.local>
2026-03-20 09:53:12 -0700

    added signals, hosted sonar circle rendering to level

--- project/scenes/levels/fishing/fish.gd
+++ project/scenes/levels/fishing/fish.gd
@@ -16,7 +16,6 @@ enum State {PATROLLING, FOLLOWING}
 
 
 var child_mesh: MeshInstance3D
-var highlight_circle: MeshInstance3D
 var state := State.PATROLLING
 var target_player: Node3D = null
 
@@ -47,13 +46,6 @@ func _physics_process(_delta: float) -> void:
 		behavior_policy.while_player_not_being_followed(self )
 
 
-func _process(_delta: float) -> void:
-	# If the sonar is active, highlight the fish in the lake
-	if highlight_circle and (highlight_circle.mesh as SphereMesh).radius > 0:
-		(highlight_circle.mesh as SphereMesh).radius -= 0.003
-	elif highlight_circle:
-		highlight_circle.queue_free()
-		highlight_circle = null
 
 func on_player_detected(player: Node3D) -> void:
 	print("player detected!!")
@@ -66,27 +58,7 @@ func on_player_lost(_player: Node3D) -> void:
 	target_player = null
 
 func highlight() -> void:
-	if highlight_circle:
-		return
-	var water_surface: MeshInstance3D = get_tree().get_first_node_in_group("WaterSurface")
-	var water_surface_pos: Vector3 = water_surface.global_position
-	var fish_pos: Vector3 = global_position
-	var layer_offset := 1.0
-	var intersection_y: float = fish_pos.y + water_surface_pos.y + layer_offset
-
-	highlight_circle = MeshInstance3D.new()
-	var sphere_mesh := SphereMesh.new()
-	sphere_mesh.radius = 3
-	sphere_mesh.height = 3
-	highlight_circle.mesh = sphere_mesh
-	var mat := StandardMaterial3D.new()
-	mat.albedo_color = Color.RED
-	mat.emission_energy_multiplier = 5.0
-	mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	highlight_circle.set_surface_override_material(0, mat)
-
-	get_tree().root.add_child(highlight_circle)
-	highlight_circle.global_position = Vector3(fish_pos.x, intersection_y, fish_pos.z)
+	signalbus.sonar_highlight.emit(global_position)
 
 
 func play_animation(animation: String) -> void:
--- project/scenes/levels/fishing/fishing_level.gd
+++ project/scenes/levels/fishing/fishing_level.gd
@@ -6,7 +6,18 @@ const NUM_OBJECTS := 80
 const RNG_SEED := 42
 
 
+# Used for rendering sonar highlights
+var _highlight_circles: Array[MeshInstance3D] = []
+var _highlight_mat: StandardMaterial3D
+
+
 func _ready() -> void:
+	_highlight_mat = StandardMaterial3D.new()
+	_highlight_mat.albedo_color = Color.RED
+	_highlight_mat.emission_energy_multiplier = 5.0
+	_highlight_mat.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+
+	var _err: int = signalbus.sonar_highlight.connect(_on_sonar_highlight)
 	_populate_sea_floor()
 
 
@@ -82,3 +93,35 @@ func _populate_sea_floor() -> void:
 		mesh_inst.position = pos
 		mesh_inst.rotation.y = rng.randf() * TAU
 		parent.add_child(mesh_inst)
+
+
+func _process(_delta: float) -> void:
+	var i := 0
+	while i < _highlight_circles.size():
+		var circle := _highlight_circles[i]
+		var disc := circle.mesh as CylinderMesh
+		if disc.top_radius > 0.0:
+			disc.top_radius -= 0.003
+			disc.bottom_radius -= 0.003
+			i += 1
+		else:
+			circle.queue_free()
+			_highlight_circles.remove_at(i)
+
+
+func _on_sonar_highlight(fish_position: Vector3) -> void:
+	var water_surface: MeshInstance3D = get_tree().get_first_node_in_group("WaterSurface")
+	var surface_y: float = water_surface.global_position.y if water_surface else 0.0
+	var highlight_y: float = surface_y + 1.0
+
+	var circle := MeshInstance3D.new()
+	var disc := CylinderMesh.new()
+	disc.top_radius = 3.0
+	disc.bottom_radius = 3.0
+	disc.height = 0.05
+	circle.mesh = disc
+	circle.set_surface_override_material(0, _highlight_mat)
+
+	add_child(circle)
+	circle.global_position = Vector3(fish_position.x, highlight_y, fish_position.z)
+	_highlight_circles.append(circle)
