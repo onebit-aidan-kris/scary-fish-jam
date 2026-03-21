2d7022a3bfa021d456a2058ad3dd2e3c9b3d954d
Aidan Fleischer <aidan@Aidans-MacBook-Air.local>
2026-03-21 16:19:21 -0700

    to simplify net casting even more

--- project/scenes/levels/fishing/boat.gd
+++ project/scenes/levels/fishing/boat.gd
@@ -13,7 +13,7 @@ var _cam_origin_pitch: float
 
 var sonar_cooldown_ticks: int = 0
 var sonar_cooldown_max: int = 120
-var net_position: Vector3 = Vector3.ZERO
+var net_local_offset: Vector3 = Vector3.ZERO
 var net_debug_mesh: MeshInstance3D = null
 
 enum NetState {NONE, AIMING, NETTING, NETTED}
@@ -38,8 +38,7 @@ func _physics_process(delta: float) -> void:
 			move_boat(_input.move.x, _input.move.y, delta)
 		if Input.is_action_just_pressed("net_arm"):
 			net_state = NetState.AIMING
-			net_position = global_position + (-global_transform.basis.z) * 10.0
-			net_position.y = global_position.y
+			net_local_offset = Vector3(0.0, 0.0, -4.0)
 		return
 
 	if net_state == NetState.AIMING:
@@ -61,17 +60,21 @@ func receive_damage(damage_amount: int) -> void:
 		print("Player is dead!")
 
 
+func get_flat_position() -> Vector2:
+	var pos_flat := Vector2(position.x, position.z)
+	if pos_flat.length() > lake_radius:
+		pos_flat = pos_flat.normalized() * lake_radius
+	return pos_flat
+
 func move_boat(x_move, y_move, delta: float) -> void:
 	rotation.y -= x_move * turn_speed * delta
 
 	var forward := -transform.basis.z
 	position += forward * (-y_move) * move_speed * delta
 
-	var pos_flat := Vector2(position.x, position.z)
-	if pos_flat.length() > lake_radius:
-		pos_flat = pos_flat.normalized() * lake_radius
-		position.x = pos_flat.x
-		position.z = pos_flat.y
+	var pos_flat := get_flat_position()
+	position.x = pos_flat.x
+	position.z = pos_flat.y
 
 	_camera.rotation_degrees.x = clampf(
 		_cam_origin_pitch + x_move,
@@ -90,8 +93,7 @@ func aim_net() -> void:
 	# 
 	var should_move_left: bool = Input.is_action_pressed("move_left")
 	var should_move_right: bool = Input.is_action_pressed("move_right")
-	var should_move_forward: bool = Input.is_action_pressed("move_forward")
-	var should_move_backward: bool = Input.is_action_pressed("move_backward")
+
 
 	var next_left_right_position: int = 0
 	var next_forward_position: int = 0
@@ -101,15 +103,11 @@ func aim_net() -> void:
 	if should_move_right:
 		next_left_right_position += 1
 
-	if should_move_forward:
-		next_forward_position += 1
-
-	if should_move_backward:
-		next_forward_position -= 1
+	next_forward_position += 1
 
 	var x_move: float = float(next_left_right_position)
-	var y_move: float = float(next_forward_position)
-	move_boat(x_move, y_move, 0.01)
+	var dummy_delta: float = Engine.time_scale / float(Engine.physics_ticks_per_second)
+	move_boat(x_move, 0, dummy_delta)
 
 
 	# If moving left or right, move the boat itself left or right.
@@ -117,32 +115,33 @@ func aim_net() -> void:
 		pass
 
 
-	net_position += global_transform.basis.x * x_move * 0.1
-	net_position += -global_transform.basis.z * y_move * 0.1
-	net_position.y = global_position.y
+	net_local_offset.x += x_move * 0.1
+
+	var net_world_pos: Vector3 = global_transform * net_local_offset
+	net_world_pos.y = global_position.y
+
 	if not net_debug_mesh:
 		set_net_debug_mesh(net_debug_mesh)
 	else:
-		net_debug_mesh.global_position = net_position
-	# Ensures  the world-space vertex positions in the arc aren't offset by the boat's own transform
+		net_debug_mesh.global_position = net_world_pos
 
-	print("net_position: ", net_position)
 	net_arc.global_transform = Transform3D.IDENTITY
-	net_arc.mesh = net_arc.call("calculate_net_path", global_position, net_position)
+	net_arc.mesh = net_arc.call("calculate_net_path", global_position, net_world_pos)
 
 
 func retract_net() -> void:
 	net_state = NetState.NONE
 	clear_net_debug_mesh()
-	net_position = Vector3.ZERO
+	net_local_offset = Vector3.ZERO
+	net_arc.mesh = null
 
 
-func set_net_debug_mesh(mesh: MeshInstance3D) -> void:
+func set_net_debug_mesh(_mesh: MeshInstance3D) -> void:
 	net_debug_mesh = MeshInstance3D.new()
 	net_debug_mesh.mesh = CylinderMesh.new()
 	net_debug_mesh.set_surface_override_material(0, StandardMaterial3D.new())
-	net_debug_mesh.global_position = net_position
-	add_child(net_debug_mesh)
+	net_debug_mesh.global_position = global_transform * net_local_offset
+	get_tree().root.add_child(net_debug_mesh)
 
 
 func clear_net_debug_mesh() -> void:
